cabffdcdc2c16d313af1e914d151f8706e96873f
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-07-26 11:24:54 -0500

    test

--- server/server.js
+++ server/server.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
-app.listen(4000, ()=> console.log(`server running on 4000`))
+app.listen(4000, ()=> console.log(`server running on 4000`));
